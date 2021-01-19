d9828284486cdf7a7c737b9b23093bb158402bba
alizainbr <70113078+alizainbr@users.noreply.github.com>
2021-01-19 18:30:07 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product');
-const Category = require('../models/Category');
+const Product = require('../models/product');
+const Category = require('../models/category');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
