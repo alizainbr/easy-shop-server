b703abb1d06d38e678654522bdc4d47d05d218d5
alizainbr <70113078+alizainbr@users.noreply.github.com>
2021-01-19 18:29:37 +0500

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,4 @@
-const Order = require('../models/Order');
+const Order = require('../models/order');
 const OrderItem = require('../models/OrderItem');
 const express = require('express');
 const router = express.Router();
